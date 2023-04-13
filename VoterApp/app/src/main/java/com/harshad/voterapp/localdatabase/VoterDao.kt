0bbbebc378e64fcb8293483287816c0ec46509f4
SpartanHarshad <harshadpawar8555@gmail.com>
2023-04-13 12:54:03 +0530

    DAO operation done

---
+++ VoterApp/app/src/main/java/com/harshad/voterapp/localdatabase/VoterDao.kt
@@ -0,0 +1,34 @@
+package com.harshad.voterapp.localdatabase
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VoterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun registerUser(userEntity: UserEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveVote(voterBoardEntity: VoterBoardEntity)
+
+    @Query("SELECT * FROM voter_board WHERE u_email = :email")
+    suspend fun checkUserAlreadyVoted(email: String): LiveData<Int>
+
+    @Query("SELECT SUM (can_1) FROM voter_board")
+    suspend fun getVoteCountFirstCandidate(): LiveData<Double>
+
+    @Query("SELECT SUM (can_2) FROM voter_board")
+    suspend fun getVoteCountSecondCandidate(): LiveData<Double>
+
+    @Query("SELECT SUM (can_3) FROM voter_board")
+    suspend fun getVoteCountThirdCandidate(): LiveData<Double>
+
+    @Query("SELECT SUM (can_4) FROM voter_board")
+    suspend fun getVoteCountFourthCandidate(): LiveData<Double>
+
+
+}
