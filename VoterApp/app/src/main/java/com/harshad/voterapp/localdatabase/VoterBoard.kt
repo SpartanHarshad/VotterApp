9f9ced9e1ccfb609a79c1ac5f0780d5bface27ee
SpartanHarshad <harshadpawar8555@gmail.com>
2023-04-13 12:30:34 +0530

    voter board added

---
+++ VoterApp/app/src/main/java/com/harshad/voterapp/localdatabase/VoterBoard.kt
@@ -0,0 +1,23 @@
+package com.harshad.voterapp.localdatabase
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "voter_board")
+data class VoterBoard(
+    @ColumnInfo(name = "can_1")
+    var can1: String,
+    @ColumnInfo(name = "can_2")
+    var can2: String,
+    @ColumnInfo(name = "can_3")
+    var can3: String,
+    @ColumnInfo(name = "can_4")
+    var can4: String,
+    @ColumnInfo(name = "u_email")
+    var uEmail: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "vId")
+    var vId: Int? = null
+}
